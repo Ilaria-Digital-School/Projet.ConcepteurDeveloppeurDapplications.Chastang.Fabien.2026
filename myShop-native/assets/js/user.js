09abf1c42618e54613d1b4849bd8c5d5b4b24cef
fabien-chastang <chastangfabien0@gmail.com>
2026-07-16 12:06:24 +0200

    Populate the list of countries

--- myShop-native/assets/js/user.js
+++ myShop-native/assets/js/user.js
@@ -120,15 +120,28 @@ class Country {
 
     static other = { value: 0, label: "– Autre –" };
     static list = [
-        { value: 1, label: "Canada" },
-        { value: 2, label: "Chine" },
-        { value: 3, label: "Espagne" },
-        { value: 4, label: "États-Unis" },
-        { value: 5, label: "France" },
-        { value: 6, label: "Italie" },
-        { value: 7, label: "Japon" },
-        { value: 8, label: "Royaume-Uni" },
-        { value: 9, label: "Tunisie" },
+        { value: 1, label: "Allemagne" },
+        { value: 2, label: "Autriche" },
+        { value: 3, label: "Belgique" },
+        { value: 4, label: "Brésil" },
+        { value: 5, label: "Canada" },
+        { value: 6, label: "Chine" },
+        { value: 7, label: "Espagne" },
+        { value: 8, label: "États-Unis" },
+        { value: 9, label: "France" },
+        { value: 10, label: "Grèce" },
+        { value: 11, label: "Inde" },
+        { value: 12, label: "Italie" },
+        { value: 13, label: "Japon" },
+        { value: 14, label: "Luxembourg" },
+        { value: 15, label: "Mauritanie" },
+        { value: 16, label: "Mexique" },
+        { value: 17, label: "Portugal" },
+        { value: 18, label: "Royaume-Uni" },
+        { value: 19, label: "Sénégal" },
+        { value: 20, label: "Suisse" },
+        { value: 21, label: "Taïwan" },
+        { value: 22, label: "Tunisie" },
         this.other
     ];
 
