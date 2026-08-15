34cb96aaf281913cca514fb41e664144be59d6a6
fabien-chastang <chastangfabien0@gmail.com>
2026-08-15 11:50:51 +0200

    Addition of a function that returns a random string

--- myShop-angular/src/app/models/common.ts
+++ myShop-angular/src/app/models/common.ts
@@ -1,22 +1,87 @@
 export class Common {
-  // Returns an 11-character alphanumeric identifier
-  static getID() {
-    const LENGTH = 11;
-    const randChar = (min: number, width: number) =>
-      String.fromCharCode(min + Math.floor(width * Math.random()));
+  // Returns a random string of characters
+  // 'type' parameter:
+  //    1 == Uppercase letters only
+  //    2 == Lowercase letters only
+  //    3 == Letters only
+  //    4 == Digits only
+  //    5 == Alphanumeric characters only (no underscores)
+  //    0 == Printable ASCII characters (character codes 33–126)
+  //
+  static randomString(length: number, type: number = 5, additionalChars: string[] = []) {
+    const randChar = (start: number, interval: number) =>
+      String.fromCharCode(start + Math.floor(interval * Math.random()));
+
+    let interval!: number;
+    let func: () => string;
+
+    switch (type) {
+      case 1:
+        // Uppercase letters only
+        interval = 26;
+        func = () => randChar(65, 26);
+        break;
+      case 2:
+        // Lowercase letters only
+        interval = 26;
+        func = () => randChar(97, 26);
+        break;
+      case 3:
+        // Letters only
+        interval = 52;
+        func = () => {
+          let choice = Math.floor(2 * Math.random());
+          return choice === 0 ? randChar(65, 26) : randChar(97, 26);
+        };
+        break;
+      case 4:
+        // Digits only
+        interval = 10;
+        func = () => randChar(48, 10);
+        break;
+      case 5:
+        // Alphanumeric characters only (no underscores)
+        interval = 62;
+        func = () => {
+          let choice = Math.floor(62 * Math.random());
+          if (choice < 26) {
+            return randChar(65, 26); // Uppercase letter
+          } else if (choice < 52) {
+            return randChar(97, 26); // Lowercase letter
+          } else {
+            return randChar(48, 10); // Digit
+          }
+        };
+        break;
+      default:
+        // Printable ASCII characters (character codes 33–126)
+        interval = 94;
+        func = () => randChar(33, 94);
+    }
 
     let ID = '';
-    for (let i = 0; i < LENGTH; i++) {
-      const TYPE = Math.floor(62 * Math.random());
-      if (TYPE < 26) {
-        ID += randChar(65, 26); // Uppercase letter
-      } else if (TYPE < 52) {
-        ID += randChar(97, 26); // Lowercase letter
-      } else {
-        ID += randChar(48, 10); // Digit
+    let addChrLen = additionalChars.length;
+
+    if (addChrLen > 0) {
+      for (let i = 0; i < length; i++) {
+        let choice = Math.floor((interval + addChrLen) * Math.random());
+        if (choice < interval) {
+          ID += func();
+        } else {
+          let rank = Math.floor(addChrLen * Math.random());
+          ID += additionalChars[rank];
+        }
       }
+    } else {
+      for (let i = 0; i < length; i++) ID += func();
     }
+
     return ID;
+  }
+
+  // Returns an 11-character alphanumeric identifier
+  static getID() {
+    return this.randomString(11);
   }
 
   // Converts a string to a number by using the current or specified locale settings
